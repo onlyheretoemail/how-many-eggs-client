773922f4517574b7202f2460ea717956e4f34d78
Zachary Temkin <zachtemkin@gmail.com>
2023-06-21 21:43:34 -0400

    begins building form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,18 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <h1>How many eggs</h1>
+        <h1>How many eggs could you eat in an hour</h1>
+        <form action='/' method='post'>
+          <input
+            className={styles.numeggs}
+            type='number'
+            id='num-eggs'
+            name='num-eggs'
+            min='0'
+            placeholder='BIG NUMBER ENTER AREA'
+          />
+          <button type='submit'>Submit</button>
+        </form>
       </div>
     </main>
   );
